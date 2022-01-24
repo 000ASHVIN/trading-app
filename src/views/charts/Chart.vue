7b6ebc49a9885278750f3e2e12751b88d5866f00
Ashvin Solanki <ashvinnite0002@gmail.com>
2022-01-24 21:37:49 +0530

    chart changes

--- src/views/charts/Chart.vue
+++ src/views/charts/Chart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div v-if="tradesData.length > 0">{{tradesData}}</div>
     <line-chart
+      v-if="tradesData.length > 0"
       :chart-data="tradesData"
       :options="chartOptions"
       label="Trades"
@@ -10,8 +10,9 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+// import { mapActions, mapGetters } from 'vuex'
 import LineChart from '@/views/charts/LineChart.vue'
+import axios from "axios"
 
 export default {
   name: 'Chart',
@@ -26,22 +27,19 @@ export default {
         maintainAspectRatio: false,
       },
       showChart: false,
+      url: 'http://localhost:3000',
     }
   },
-  computed: {
-    ...mapGetters(['trades']),
-  },
-  created() {
-    this.getTrades()
-    this.trades.forEach(data => {
+  async mounted() {
+    let trades = await axios.get(this.url + '/trades_list')
+    trades = trades.data
+    trades.forEach(data => {
       const date = data.trade_time
       const { price } = data
 
       this.tradesData.push({ date, price })
     })
   },
-  methods: {
-    ...mapActions(['getTrades']),
-  },
+  methods: {},
 }
 </script>
